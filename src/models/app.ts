f83bc41a3d0e730ba152f2da04092142b9a58904
Na Vu <vuthilyna304@gmail.com>
2020-08-20 10:37:54 +0700

    add token to localstorage

--- src/models/app.ts
+++ src/models/app.ts
@@ -1,73 +1,4 @@
-// // import service from './services';
-// // import {apiLogin} from '../services/api.js'
-// import service from './services';
-// import { Effect, Reducer } from 'umi';
-// // import axios from 'axios';
 
-// export interface LoginState {
-//   name: string,
-//   phone: string,
-//   password :string,
-//   isLogin: boolean
-// }
-
-
-// export interface LoginModelType {
-//   namespace: string,
-//   state: LoginState,
-//   effects:{
-//     login:Effect;
-//     fetch:Effect;
-//   }
-//   reducers:{
-//     save:Reducer<LoginState>;
-//   }
-// }
-// const LoginModel : LoginModelType= {
-//   namespace: 'login',
-//   state: {
-//     name:"",
-//     phone:"",
-//     password:"",
-//     isLogin: false
-//   },
-  
-//   effects: {
-//     *submitlogin( {payload},{call, put, select}){
-//       console.log('----------call effect -----')
-//      const {response} = yield call(service.postLogin, payload);
-//      console.log(response)
-//     // --------
-
-//       yield put({
-//         type: 'fetch',
-//         payload ,//với lại cai token chộ này nữa
-//       })
-//     },
-//     *fetch({ payload }, { call, put }) {
-//       const { data } = yield call(service.getLogin);
-//       yield put({
-//         type: 'save',
-//         payload: {
-//           list: data,// chặp là email,pass
-//         },
-//       });
-//     }
-//   },
-
-//   reducers:{
-//     save(state, action){
-//       console.log('----------call reducer --save---');
-//       console.log(state);
-//       console.log(action)
-//       return {
-//         ...state,
-//         ...action.payload,
-//       }
-//     }
-//   }
-// }
-// export default LoginModel;
 import service from './services';
 import { Effect, Reducer } from 'umi';
 // import axios from 'axios';
@@ -88,7 +19,6 @@ export interface LoginState {
   email: string,
   password :string,
   isLogin: boolean,
-  token:any
 }
 
 
@@ -108,7 +38,6 @@ const LoginModel : LoginModelType= {
     email:"",
     password:"",
     isLogin: false,
-    token:null
   },
   
   effects: {
@@ -117,34 +46,32 @@ const LoginModel : LoginModelType= {
      const response = yield call(service.postLogin, payload);
      console.log(response.data, 'respone login này')
 var dataLogin =response.data;
-//     const token = yield call(loginApi, payload)
-// console.log('token nè: =>', token)
-    // --------
 
-//=--------------
       yield put({
         type: 'save',
-        dataLogin ,//với lại cai token chộ này nữa
+        dataLogin ,
       })
     },
 
   },
   
   reducers:{
-    save(state, action, token){
+    save(state, action){
       console.log('----------call reducer --save---');
       console.log(state, 'state ở reducer này');
       console.log(action.dataLogin, 'action ở reducer này')
     const a=action.dataLogin;
     const b = a.user ;
     const c= a.token
-    console.log('token, ',c)
+    console.log(b)
+    localStorage.setItem("accessToken", c);
+    localStorage.setItem("user", JSON.stringify(b));
       return {
         ...b,
         ...action.payload,// hay là action.datalogin ?
-        ...c
       }
     }
   }
 }
 export default LoginModel;
+//khi nào link qua trang logout thì xóa đi
